e85ef687c1cf1195eeeff884518672d603d6d7f4
Crowdbotics <team@crowdbotics.com>
2020-09-11 15:24:34 +0000

    Update components

--- src/features/BlankScreen810655/screens/blank.js
+++ src/features/BlankScreen810655/screens/blank.js
@@ -22,10 +22,52 @@ export class _Blank extends React.Component {
     }
   }
 
-  state = {}
+  state = { Input_5: "" }
 
   render = () => (
     <View>
+      <Input
+        placeholder="Sample text input placeholder"
+        editable={true}
+        textStyle={{
+          fontSize: 12,
+          color: "#000000",
+          textAlign: "left",
+          fontFamily: "Roboto-Regular"
+        }}
+        style={{
+          width: "100%",
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 5,
+          marginBottom: 5,
+          paddingLeft: 5,
+          paddingRight: 5,
+          paddingTop: 5,
+          paddingBottom: 5,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: 12,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontFamily: "Roboto-Regular",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          textDecorationLine: "none",
+          textTransform: "none",
+          lineHeight: 12,
+          letterSpacing: 0
+        }}
+        value={this.state.Input_5}
+        onChangeText={nextValue => this.setState({ Input_5: nextValue })}
+      />
       <Text style={{ color: "#db1414" }}>This is my text element</Text>
     </View>
   )
